7d4c346e94eadaaca73cf0f871faedbb33349773
Tanel Pauls <paulstanel@protonmail.com>
2025-09-05 10:21:01 +0300

    Ülesanne 1 tehtud

---
+++ src/Ylesanded.java
@@ -0,0 +1,8 @@
+public class Ylesanded {
+    public static void main(String[] args) {
+        ylesanne1();
+    }
+    public static void ylesanne1() {
+        System.out.println("Tanel Pauls");
+    }
+}
