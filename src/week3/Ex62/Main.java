3e1b2a9eefd9a538aaa9c167f89ae738878e2454
Tanel Pauls <paulstanel@protonmail.com>
2025-09-18 12:31:20 +0300

    Exercise 62

---
+++ src/week3/Ex62/Main.java
@@ -0,0 +1,31 @@
+package week3.Ex62;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> brothers = new ArrayList<String>();
+        brothers.add("Dick");
+        brothers.add("Henry");
+        brothers.add("Michael");
+        brothers.add("Bob");
+
+        System.out.println("brothers:");
+        System.out.println(brothers);
+
+// sorting brothers
+        Collections.sort(brothers);
+
+// removing the last item
+        removeLast(brothers);
+
+        System.out.println(brothers);
+    }
+
+    public static void removeLast(ArrayList<String> list) {
+        if (!list.isEmpty()) {
+            list.remove(list.size() - 1);
+        }
+    }
+}
