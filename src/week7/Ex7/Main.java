858bc5442ca9bc9733958c2fe66a02c3294191ad
Tanel Pauls <paulstanel@protonmail.com>
2025-09-18 15:31:25 +0300

    Exercise 7, week 7

--- src/week7/Ex7/Main.java
+++ src/week7/Ex7/Main.java
@@ -5,26 +5,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //7.1
+        /*Dictionary dictionary = new Dictionary();
+        dictionary.add("apina", "monkey");
+        dictionary.add("banaani", "banana");
+        dictionary.add("cembalo", "harpsichord");
+
+        System.out.println(dictionary.translate("apina"));
+        System.out.println(dictionary.translate("porkkana"));*/
+
+        //7.2
         /*Dictionary dictionary = new Dictionary();
         dictionary.add("apina", "monkey");
         dictionary.add("banaani", "banana");
         System.out.println(dictionary.amountOfWords());
 
         dictionary.add("cembalo", "harpsichord");
-        System.out.println(dictionary.amountOfWords());
+        System.out.println(dictionary.amountOfWords());*/
+
+        //7.3
+        /*Dictionary dictionary = new Dictionary();
+        dictionary.add("apina", "monkey");
+        dictionary.add("banaani", "banana");
+        dictionary.add("cembalo", "harpsichord");
 
         ArrayList<String> translations = dictionary.translationList();
         for(String translation: translations) {
             System.out.println(translation);
         }*/
 
-
-        /*Scanner reader = new Scanner(System.in);
+        //7.4
+/*        Scanner reader = new Scanner(System.in);
         Dictionary dictionary = new Dictionary();
 
         TextUserInterface ui = new TextUserInterface(reader, dictionary);
         ui.start();*/
 
+        //7.5
         String input = "add\n"  + "cheese\n" + "juusto\n" +
                 "add\n"  + "bier\n" + "olut\n" +
                 "add\n"  + "book\n" + "kirja\n" +
