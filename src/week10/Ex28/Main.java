335902ac265dcf06e2ca42bb8f3f783f2dcb792a
Tanel Pauls <paulstanel@protonmail.com>
2025-09-25 10:02:08 +0300

    week 10, exercise 28

---
+++ src/week10/Ex28/Main.java
@@ -0,0 +1,24 @@
+package week10.Ex28;
+
+import week10.Ex28.Movable.Group;
+import week10.Ex28.Movable.Organism;
+
+public class Main {
+    public static void main(String[] args) {
+        //28.1
+        /*Organism organism = new Organism(20, 30);
+        System.out.println(organism);
+        organism.move(-10, 5);
+        System.out.println(organism);
+        organism.move(50, 20);
+        System.out.println(organism);*/
+
+        //28.2
+        Group group = new Group();
+        group.addToGroup(new Organism(73, 56));
+        group.addToGroup(new Organism(57, 66));
+        group.addToGroup(new Organism(46, 52));
+        group.addToGroup(new Organism(19, 107));
+        System.out.println(group);
+    }
+}
---
+++ src/week10/Ex28/Movable/Group.java
@@ -0,0 +1,32 @@
+package week10.Ex28.Movable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group implements Movable {
+    private List<Movable> members;
+
+    public Group() {
+        this.members = new ArrayList<>();
+    }
+
+    public void addToGroup(Movable movable) {
+        this.members.add(movable);
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        for (Movable m : members) {
+            m.move(dx, dy);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Movable m : members) {
+            sb.append(m).append("\n");  // works for both Organism and Group
+        }
+        return sb.toString();
+    }
+}
